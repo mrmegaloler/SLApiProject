48d6ef3b174133cb7e44778023ce5278f00909ee
ad26505 <ad26505@WK080990.ad.stockholm.se>
2016-04-15 11:09:07 +0200

    fixed some bugs
    and created some new ones
    
    also fixed the layout and changed to tab system
    
    fix the bug with not displaying times

--- apiTest/src/apiTest/ReadXML.java
+++ apiTest/src/apiTest/ReadXML.java
@@ -11,72 +11,111 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+
+
 public class ReadXML {
+	
+	public static Document doc; 
+	
+	public static void XMLGen(String direction) throws ParserConfigurationException, SAXException, IOException {
+	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	DocumentBuilder builder = factory.newDocumentBuilder();
+	String URLB = new String(
+			"http://api.sl.se/api2/realtimedepartures.xml?key=e082a3a9b23b418ebc63266c6c0480bd&timewindow=20&siteid=");
+	String URLC = URLB + URLParser.GetURL(direction);
+	doc = builder.parse(URLC);
+	
+	}
+	
 	public static String timeleft(String direction) throws ParserConfigurationException, SAXException, IOException {
 		int n = 0;
 		boolean isMetro = false;
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder builder = factory.newDocumentBuilder();
-		String URLB = new String(
-				"http://api.sl.se/api2/realtimedepartures.xml?key=e082a3a9b23b418ebc63266c6c0480bd&timewindow=20&siteid=");
-		String URLC = URLB + URLParser.GetURL(direction);
-		Document doc = builder.parse(URLC);
+		
 		NodeList DisplayTime = doc.getElementsByTagName("DisplayTime");
 		NodeList transportMode = doc.getElementsByTagName("TransportMode");
+		int departureNo = transportMode.getLength();
 		NodeList Direction = doc.getElementsByTagName("JourneyDirection");
 		NodeList Line = doc.getElementsByTagName("GroupOfLineId");
 		Element timeleft = null;
+		
+		String timeLeft = "x";
+		
 		do {
-			Element transportMode1 = (Element) transportMode.item(n + visualApi.pos);
-			Element Direction1 = (Element) Direction.item(n + visualApi.pos);
-			System.out.println(transportMode1.getFirstChild().getNodeValue());
-			String transportText = new String(transportMode1.getFirstChild().getNodeValue());
+			Element transportMode1 = (Element) transportMode.item(n);
+			Element Direction1 = (Element) Direction.item(n);
+			//System.out.println(transportMode1.getFirstChild().getNodeValue());
+			String transportText = "error";
+			if(n<departureNo){
+			transportText = new String(transportMode1.getFirstChild().getNodeValue());
 			String directionText = new String(Direction1.getFirstChild().getNodeValue());
-			String text = new String("METRO");
-			if (transportText.equals(text)) {
-				timeleft = (Element) DisplayTime.item(n + visualApi.pos);
+			}
+			        
+			if (transportText.equals("Metro")) {
+				timeleft = (Element) DisplayTime.item(n);
+				timeLeft = timeleft.getFirstChild().getNodeValue();
 				isMetro = true;
-			} else if (transportMode1.getFirstChild().getNodeValue() != text) {
+			} else if (transportText != "Metro" && n<=departureNo) {
 				n++;
 				isMetro = false;
+			} else if (n>departureNo){
+				timeLeft = ("No subway departures from here.");
+				isMetro = true;
 			}
 		} while (isMetro == false);
 		
 		n=0;
-		return timeleft.getFirstChild().getNodeValue();
+		
+		
+		return timeLeft;
 
 	}
 
 	public static String departure(String direction) throws SAXException, IOException, ParserConfigurationException {
 		int n = 0;
 		boolean isMetro = false;
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder builder = factory.newDocumentBuilder();
-		String URLB = new String(
-				"http://api.sl.se/api2/realtimedepartures.xml?key=e082a3a9b23b418ebc63266c6c0480bd&timewindow=10&siteid=");
-		String URLC = URLB + URLParser.GetURL(direction);
-		Document doc = builder.parse(URLC);
+		
+		
+		//Get the nodes with tag name "Destination"
 		NodeList Destination = doc.getElementsByTagName("Destination");
 		Element destination1 = null;
+		//gets the nodes with tag name "transportmode"
 		NodeList transportMode = doc.getElementsByTagName("TransportMode");
+		int departureNo = transportMode.getLength();
+		
+		NodeList Direction = doc.getElementsByTagName("JourneyDirection");
+		
+		String timeLeft = "x";
 		
 		do {
-			Element transportMode1 = (Element) transportMode.item(n + visualApi.pos);
-			System.out.println(transportMode1.getFirstChild().getNodeValue());
-			String transportText = new String(transportMode1.getFirstChild().getNodeValue());
+			//creates an element from the nth item in the transportmode nodelist
+			Element transportMode1 = (Element) transportMode.item(n);
+			Element Direction1 = (Element) Direction.item(n);
+			//System.out.println(transportMode1.getFirstChild().getNodeValue());
+			String transportText = "Error";
 			String text = new String("METRO");
+			
+			if(n<departureNo){
+				transportText = new String(transportMode1.getFirstChild().getNodeValue());
+				String directionText = new String(Direction1.getFirstChild().getNodeValue());
+				
+			}
+			
 			if (transportText.equals(text)) {
-				destination1 = (Element) Destination.item(n + visualApi.pos);
+				destination1 = (Element) Destination.item(n);
+				timeLeft = destination1.getFirstChild().getNodeValue();
 				isMetro = true;
-			} else if (transportMode1.getFirstChild().getNodeValue() != text) {
+			} else if (transportText != text && n<=departureNo) {
 				n++;
 				isMetro = false;
+			} else if (n>departureNo){
+				System.out.print("error 1");
+				isMetro = true;
 			}
 		} while (isMetro == false);
 		
 		n=0;
 
-		return destination1.getFirstChild().getNodeValue();
+		return timeLeft;
 
 	}
 
--- apiTest/src/apiTest/visualApi.java
+++ apiTest/src/apiTest/visualApi.java
@@ -18,16 +18,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.HashMap;
+import javax.swing.JTabbedPane;
 
 public class visualApi extends JFrame implements ActionListener {
 	JButton btnGetrefresh = new JButton("Get departures");
 	JLabel lblTimeLeft = new JLabel("");
-	JLabel Departure1 = new JLabel("");
+	static JLabel Departure1 = new JLabel("");
 	JLabel Departure2 = new JLabel("");
 	JLabel TimeLeft1 = new JLabel("");
 	JLabel TimeLeft2 = new JLabel("");
 	public static JLabel GetStation = new JLabel("");
-	public static int pos = 0;
 	HashMap <Integer, JLabel> boxt= new HashMap <Integer, JLabel>();
 	
 	HashMap <Integer, JLabel> boxd= new HashMap <Integer, JLabel>();
@@ -58,19 +58,39 @@ public class visualApi extends JFrame implements ActionListener {
 	 */
 	public visualApi() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 551, 358);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(10, 89, 525, 231);
+		contentPane.add(tabbedPane);
+		
+		JPanel MetroPanel = new JPanel();
+		tabbedPane.addTab("Metro", null, MetroPanel, null);
+		MetroPanel.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		tabbedPane.addTab("New tab", null, panel_1, null);
+		panel_1.setLayout(null);
+		
+		JPanel panel_2 = new JPanel();
+		tabbedPane.addTab("New tab", null, panel_2, null);
+		panel_2.setLayout(null);
+		
+		JPanel panel_3 = new JPanel();
+		tabbedPane.addTab("New tab", null, panel_3, null);
+		panel_3.setLayout(null);
+		
 	
-		btnGetrefresh.setBounds(179, 11, 245, 42);
+		btnGetrefresh.setBounds(280, 11, 245, 42);
 		contentPane.add(btnGetrefresh);
 		
 		txtStation = new JTextField();
 		txtStation.setText("Station");
-		txtStation.setBounds(10, 11, 159, 42);
+		txtStation.setBounds(10, 11, 260, 42);
 		contentPane.add(txtStation);
 		txtStation.setColumns(10);
 		
@@ -79,25 +99,25 @@ public class visualApi extends JFrame implements ActionListener {
 		contentPane.add(lblTimeLeft);
 		
 		
-		Departure1.setBounds(10, 157, 144, 14);
-		contentPane.add(Departure1);
+		Departure1.setBounds(10, 11, 144, 14);
+		MetroPanel.add(Departure1);
 		
 		
-		Departure2.setBounds(10, 220, 144, 14);
-		contentPane.add(Departure2);
+		Departure2.setBounds(10, 74, 144, 14);
+		MetroPanel.add(Departure2);
 		
-		TimeLeft1.setBounds(179, 157, 116, 14);
-		contentPane.add(TimeLeft1);
+		TimeLeft1.setBounds(179, 11, 245, 14);
+		MetroPanel.add(TimeLeft1);
 		
-		TimeLeft2.setBounds(179, 220, 116, 14);
-		contentPane.add(TimeLeft2);
+		TimeLeft2.setBounds(179, 74, 245, 14);
+		MetroPanel.add(TimeLeft2);
 		
 		JLabel lblStation = new JLabel("Showing departures from: ");
-		lblStation.setBounds(10, 81, 159, 14);
+		lblStation.setBounds(10, 64, 159, 14);
 		contentPane.add(lblStation);
 		
 		
-		GetStation.setBounds(179, 81, 245, 14);
+		GetStation.setBounds(179, 64, 245, 14);
 		contentPane.add(GetStation);
 		
 		btnGetrefresh.addActionListener(this);
@@ -107,14 +127,21 @@ public class visualApi extends JFrame implements ActionListener {
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		pos = 0;
+	public void actionPerformed(ActionEvent e) {		
+		
+		
 		boxt.put(1, TimeLeft1);
 		boxt.put(2, TimeLeft2);
 		boxd.put(1, Departure1);
 		boxd.put(2, Departure2);
 		
 		String station = txtStation.getText();
+		try {
+			ReadXML.XMLGen(station);
+		} catch (ParserConfigurationException | SAXException | IOException e2) {
+			// TODO Auto-generated scatch block
+			e2.printStackTrace();
+		}
 		int n = 1;
 		int m = 1;
 		try {
@@ -123,7 +150,6 @@ public class visualApi extends JFrame implements ActionListener {
 			n++;
 			boxd.get(m).setText((ReadXML.departure(station)));
 			m++;
-			pos = pos + 3;
 			}	
 	
 		URLParser.initialscan=0;
